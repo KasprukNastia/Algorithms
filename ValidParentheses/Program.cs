2842ace856f47a1cb6ef73e52a8d18918795fd5c
Anastasiia Kaspruk <akaspruk@repay.com>
2023-03-16 21:52:02 +0000

    new task

---
+++ ValidParentheses/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace ValidParentheses;
+
+internal class Program
+{
+    private static Dictionary<char, char> _pairs = new()
+    {
+        { '{', '}' },
+        { '(', ')' },
+        { '[', ']' }
+    };
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine(IsValid("(("));
+    }
+
+    public static bool IsValid(string s)
+    {
+        if (s.Length % 2 != 0)
+            return false;
+
+        var stack = new Stack<char>();
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (_pairs.TryGetValue(s[i], out var pair))
+            {
+                stack.Push(s[i]);
+            }
+            else
+            {
+                if (stack.Count == 0 || _pairs[stack.Pop()] != s[i])
+                    return false;
+            }
+        }
+
+        return stack.Count == 0;
+    }
+}
