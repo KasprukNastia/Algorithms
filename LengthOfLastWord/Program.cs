b25f09806abc6acc241407760af53a1f59977e71
Anastasiia Kaspruk <akaspruk@repay.com>
2023-03-17 19:58:58 +0000

    new task

---
+++ LengthOfLastWord/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace LengthOfLastWord;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(LengthOfLastWord("   fly me   to   the moon  "));
+    }
+
+    public static int LengthOfLastWord(string s)
+    {
+        int counter = 0;
+
+        int i = s.Length - 1;
+        while (i >= 0)
+        {
+            if (s[i] != ' ')
+                counter++;
+            if (s[i] == ' ' && counter > 0)
+                return counter;
+            i--;
+        }
+
+        return counter;
+    }
+}
