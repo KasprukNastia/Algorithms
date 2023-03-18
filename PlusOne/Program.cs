13db82563037688c26ad6337cc225480b32cfb5d
Anastasiia Kaspruk <akaspruk@repay.com>
2023-03-18 09:42:08 +0000

    resolved

---
+++ PlusOne/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace PlusOne;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        foreach (var elem in PlusOne(new []{9}))
+        {
+            Console.Write($"{elem} ");
+        }
+    }
+
+    public static int[] PlusOne(int[] digits)
+    {
+        int i = digits.Length - 1;
+        while (i >= 0 && digits[i] == 9)
+        {
+            digits[i] = 0;
+            i--;
+        }
+
+        if (i < 0)
+        {
+            var result = new int[digits.Length + 1];
+            result[0] = 1;
+            Array.Copy(digits, 0, result, 1, digits.Length);
+            return result;
+        }
+
+        digits[i] += 1;
+
+        return digits;
+    }
+}
